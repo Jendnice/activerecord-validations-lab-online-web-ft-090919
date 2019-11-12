754337cbddba592a043d8ec79191872deecbdd7f
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-12 18:25:40 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,8 +1,8 @@
 class TitleValidator < ActiveModel::Validator
   
   def validate(record)
-    unless record.email.include?("Won't Believe") || ("Secret") || ("Top[number]")
-      record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
+    unless record.title.include?("Won't Believe") || ("Secret") || ("Top[number]") || ("Guess")
+      record.errors[:title] << "Your title needs more click-bait!"
     end
   end
   
