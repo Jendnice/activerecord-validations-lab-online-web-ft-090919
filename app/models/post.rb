1fce9f7c4611c7d891471fa4ac9c9953a249df18
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-12 18:18:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,6 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, 
   
-  validates :size, inclusion: { in: %w() }
+  validates :size, inclusion: { in: %w(fiction nonfiction) }
   
 end
