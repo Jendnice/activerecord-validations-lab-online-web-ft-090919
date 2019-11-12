6159badd96eb293048191e0ff4f51c7d856f0984
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-12 18:21:47 +0000

    Automatically backed up by Learn

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,9 @@
+class TitleValidator < ActiveModel::Validator
+  
+  def validate(record)
+    unless record.email.match?(/flatironschool.com/)
+      record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
+    end
+  end
+  
+end
